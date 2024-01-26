f1be96fe8ab03a038f9add5489935785ab97c0ea
fustosierra <frodriguezruelas@gmail.com>
2024-01-25 21:13:09 -0600

    adding chrome driver

--- src/test/java/tests/baseTest/TestBase.java
+++ src/test/java/tests/baseTest/TestBase.java
@@ -1,4 +1,15 @@
 package tests.baseTest;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestBase {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+    }
 }
